e8798ae4ec1c9972c6fb6ad2e1e332ba357805bd
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-30 04:16:52 -0500

    simplest possible example should work now

--- plugin/copy-diagnostics.lua
+++ plugin/copy-diagnostics.lua
@@ -1,4 +1,1 @@
-vim.api.nvim_create_user_command("NvimCopyDiagnostics", function()
-	require("copy-diagnostics.module").setup()
-end, {})
-vim.api.nvim_create_user_command("CopyDiagnostics", require("plugin_name").hello, {})
+vim.api.nvim_create_user_command("MyFirstFunction", require("copy-diagnostics").hello, {})
