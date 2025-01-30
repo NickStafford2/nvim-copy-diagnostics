e215401c60a0dfadfa2d2a292943b28e96e9b73c
Nicholas Stafford <ngstafford1@gmail.com>
2025-01-30 03:59:33 -0500

    added config back

--- lua/nvim-copy-diagnostics/init.lua
+++ lua/nvim-copy-diagnostics/init.lua
@@ -10,6 +10,7 @@ local M = {}
 M.config = config
 
 M.setup = function(args)
+	M.config = vim.tbl_deep_extend("force", M.config, args or {})
 	vim.keymap.set("n", "<Leader>cy", function()
 		local diagnostics = vim.diagnostic.get(0, {})
 		if not diagnostics or #diagnostics == 0 then
